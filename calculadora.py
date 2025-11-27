65eb201480aaacec71e781f91c6f2a7b812c4cce
amanda lacerda <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-11-27 12:15:25 +0000

    fim

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+ int(input("preço × quantidade: "))
+ preco_item = 5.99
+ quantidade_item = 4
+
+ total = calcular_total(preco_item, quantidade_item)
+print(" Cálculo Compra ")
+print(f"Produto: R$ {preco_item:.2f} x {quantidade_item} unidades")
+print(f"Total: R$ {total:.2f}")
+
